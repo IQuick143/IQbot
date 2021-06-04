d2353cb6328ffe20689acb19790f401d668aa266
IQuick 143 <IQuick143cz@gmail.com>
2021-06-04 16:09:12 +0200

    Improved flavour texts for counters

--- modules/Counting.py
+++ modules/Counting.py
@@ -38,8 +38,11 @@ class CounterCog(commands.Cog, name='counting'):
 				modifiers += " These cans make a triangle!"
 		if greatest_common_exponent > 3:
 			modifiers += f" These cans are a {greatest_common_exponent}th power, hope you know how to stack cans in {greatest_common_exponent}D."
-		if len(factorization) == 1 and factorization[0][1] == 1:
-			modifiers += " These cans are a prime number!!!"
+		if len(factorization) == 1:
+			if factorization[0][1] == 1:
+				modifiers += " These cans are a prime number!!!"
+			else:
+				modifiers += " These cans are a power of a prime number!!!"
 		if self.can % 100 == 69:
 			modifiers += " nice."
 		if self.can % 1000 == 413:
@@ -49,7 +52,7 @@ class CounterCog(commands.Cog, name='counting'):
 	@addcan.error
 	async def addcan_error(self, ctx, error):
 		if isinstance(error, commands.CommandOnCooldown):
-			await ctx.send("Please, calm down, the carapacians are doing their best to add the cans but we need a bit of time... (please try again after {:,.2f}s)".format(error.retry_after))
+			await ctx.send("Please, calm down, the carapacians are doing their best to add the cans but we need a bit of time... (please try again after {:,.2f}s)\n\nThere are now {} cans.".format(error.retry_after, self.can))
 
 	@commands.command("john", pass_context=True, description="Adds, removes, or gives status of johns in the johnverse")
 	@commands.cooldown(1, 30, commands.BucketType.user)
@@ -58,23 +61,31 @@ class CounterCog(commands.Cog, name='counting'):
 		if command == "":
 			message += "Use john add/kill to add or remove a john\n\n"
 		elif command == "add":
+			self.john_add += 1
+			self.changed = True
+
 			modifier = ""
 			if random() < 0.1:
 				modifier += "poor little "
-			if random() < 0.04:
-				modifier += "gay "
 			if random() < 0.001:
 				modifier += "rare "
+			if random() < 0.04:
+				modifier += "gay "
 			if random() < 0.005:
 				modifier += "limited edition "
+			if self.john_add - self.john_kill == 69 and random() < 0.69:
+				modifier += "horny "
+			if self.john_add - self.john_kill == 413 and random() < 0.413:
+				modifier += "egbert "
 			if len(modifier) == 0 and random() < 0.1:
 				modifier += "completely generic "
 			if len(modifier) == 0 and random() < 0.01:
 				modifier += "extraordinarily generic "
 			message += f"You have successfully added a {modifier}john\n\n"
-			self.john_add += 1
-			self.changed = True
 		elif command == "kill":
+			self.john_kill += 1
+			self.changed = True
+
 			modifier = ""
 			bonus_sentence = ""
 			if random() < 0.1:
@@ -85,6 +96,8 @@ class CounterCog(commands.Cog, name='counting'):
 				modifier += "unimaginably "
 				if random() < 0.1:
 					bonus_sentence = " I don't even comprehend."
+			if random() < 0.01:
+				modifier += "very "
 			if random() < 0.005:
 				modifier += "full-on jack-noir "
 				if random() < 0.3:
@@ -93,13 +106,11 @@ class CounterCog(commands.Cog, name='counting'):
 				modifier += "painlessly "
 				if random() < 0.08:
 					bonus_sentence = " v-v"
-			if len(modifier) == 0 and random() < 0.01:
+			if len(modifier) == 0 and random() < 0.02:
 				modifier += "accidentally "
 				if random() < 0.08:
 					bonus_sentence = " I don't believe your tales."
 			message += f"You have {modifier}killed a john.{bonus_sentence}\n\n"
-			self.john_kill += 1
-			self.changed = True
 		else:
 			message += "Uh... I am not sure how to do that, please use john add or john kill\n\n"
 		message += f"There are currently {self.john_add - self.john_kill} johns.\n{self.john_add} johns have been added, {self.john_kill} johns have been killed."
@@ -110,7 +121,11 @@ class CounterCog(commands.Cog, name='counting'):
 	@john.error
 	async def john_error(self, ctx, error):
 		if isinstance(error, commands.CommandOnCooldown):
-			await ctx.send("Please, calm down, the johniverse can only handle so many john operations per metasecond... (please try again after {:,.2f}s)".format(error.retry_after))
+			message = "Please, calm down, the johniverse can only handle so many john operations per metasecond... (please try again after {:,.2f}s)\n\n".format(error.retry_after)
+			message += f"There are currently {self.john_add - self.john_kill} johns.\n{self.john_add} johns have been added, {self.john_kill} johns have been killed."
+			if self.john_add < self.john_kill:
+				message += ".. Somehow..."
+			await ctx.send(message)
 
 	@tasks.loop(seconds=300)
 	async def update_index(self):
